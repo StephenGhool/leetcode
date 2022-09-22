e3d740ce9b93273bf1b936813cb6b9aa37c9f4d9
Stephen Ghool <55926732+StephenGhool@users.noreply.github.com>
2022-09-22 16:05:43 -0400

    Time: 39 ms (97.71%), Space: 15.3 MB (90.38%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -20,14 +20,67 @@ class Solution:
         #           2.2.1.2 yes - goto step3
         #       2.2.2 repeat step2
         
-        
-        depth = 0
+#         Recursive Method
+#         depth = 0
 
-        def dfs(node, depth):
-            if node is not None:
-                return max(dfs(node.left,depth+1), dfs(node.right,depth+1))
-            else:
-                return depth
+#         def dfs(node, depth):
+#             if node is not None:
+#                 return max(dfs(node.left,depth+1), dfs(node.right,depth+1))
+#             else:
+#                 return depth
             
         
-        return dfs(root, depth)
+#         return dfs(root, depth)
+
+        # Iterative Method
+
+        # steps:.
+        # 1. add root to stack
+        # 2. is stack node is not none 
+        #   2.1 Yes 
+        #       2.1.1 Add left and right to stack
+        #       2.1.2 Increment count
+        #   2.2 No
+        #       2.2.1 if maxcnt is less than cnt set it to cnt
+        #       2.2.2 
+        
+#         stack = [[root,1]]
+        
+#         # stores max depth
+#         max_dpt = 0
+#         cnt = 0
+#         none_cnt
+#         while(len(stack) > 0):
+#             # pop the value from the stack
+#             print(stack)
+#             node = stack.pop()
+#             print(node)
+            
+#             if node is not None:
+#                 # add neighbors to stack
+#                 stack.append(node.left)
+#                 stack.append(node.right)
+#                 cnt+=1
+#             else:
+#                 if max_dpt < cnt:
+#                     max_dpt = cnt
+#                 cnt-=1
+                
+#         return max_dpt
+
+        stack = [[root,1]]
+        max_dpt = 0
+        while(stack):
+          
+            node, depth = stack.pop()
+            
+            if node is not None:
+                # add neighbors to stack
+                stack.append([node.left,depth+1])
+                stack.append([node.right,depth+1]) 
+       
+                if max_dpt < depth:
+                    max_dpt = depth
+              
+        return max_dpt
+            
