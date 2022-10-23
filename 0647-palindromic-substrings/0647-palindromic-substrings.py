eb708041597fee67788315aa3ef4ee890dcbfdb7
Stephen Ghool <55926732+StephenGhool@users.noreply.github.com>
2022-10-23 07:22:14 -0400

    Time: 262 ms (62.26%), Space: 13.8 MB (75.75%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -3,45 +3,73 @@ class Solution:
         """
         Method 1: Two pointers
         """
+#         # edge cases
+#         if len(s) <= 1:
+#             return len(s)
+        
+#         # length of string
+#         length = len(s)
+        
+#         # store number of substrings
+#         result = 0
+        
+#         # pointers declaration
+#         left, right = 0,0
+        
+#         # odd
+#         for i, char in enumerate(s):
+#             while(left >= 0 and right <= length-1):
+#                 if s[left]==s[right]:
+#                     result += 1
+#                     left -= 1
+#                     right += 1
+#                 else:
+#                     break
+#             left = right = i
+ 
+#         # pointers declaration
+#         left, right = 0, 1
+        
+#         # even
+#         for i, char in enumerate(s):
+#             while(left >= 0 and right <= length-1):
+#                 if s[left]==s[right]:
+#                     result += 1
+#                     left -= 1
+#                     right += 1
+#                 else:
+#                     break
+            
+#             left, right = i+1, i+2
+        
+        
+#         return result
+    
+        """
+        Method 2: Two pointers condensed
+        """
+        
         # edge cases
         if len(s) <= 1:
             return len(s)
         
-        # length of string
-        length = len(s)
-        
         # store number of substrings
         result = 0
         
-        # pointers declaration
-        left, right = 0,0
-        
-        # odd
+        # even and odd
         for i, char in enumerate(s):
-            while(left >= 0 and right <= length-1):
-                if s[left]==s[right]:
-                    result += 1
-                    left -= 1
-                    right += 1
-                else:
-                    break
-            left = right = i
- 
-        # pointers declaration
-        left, right = 0, 1
-        
-        # even
-        for i, char in enumerate(s):
-            while(left >= 0 and right <= length-1):
-                if s[left]==s[right]:
-                    result += 1
-                    left -= 1
-                    right += 1
-                else:
-                    break
-            
-            left, right = i+1, i+2
-        
+            result += self.countpalindrome(s, i, i)
+            result += self.countpalindrome(s, i, i+1)
         
         return result
+            
+    def countpalindrome(self, s, left, right):
+        result = 0 
+        length = len(s)
+        while(left >= 0 and right <= length-1 and (s[left]==s[right])):
+            result += 1
+            left -= 1
+            right += 1
+        return result
+              
         
