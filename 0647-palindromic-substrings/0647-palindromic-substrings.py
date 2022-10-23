af29f5c9a617f26b89cf50729b1703a7a73d6d7a
Stephen Ghool <55926732+StephenGhool@users.noreply.github.com>
2022-10-23 07:13:45 -0400

    Time: 263 ms (61.90%), Space: 14.1 MB (24.66%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -0,0 +1,48 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        """
+        Method 1: Two pointers
+        """
+        # edge cases
+        if len(s) <= 1:
+            return len(s)
+        
+        # length of string
+        length = len(s)
+        
+        # store number of substrings
+        result = 0
+        
+        # pointers declaration
+        left, right = 0,0
+        
+        # odd
+        for i, char in enumerate(s):
+            while(left >= 0 and right <= length-1):
+                if s[left]==s[right]:
+                    result += 1
+                    left -= 1
+                    right += 1
+                else:
+                    break
+            left = right = i
+        print(result)
+        # pointers declaration
+        left, right = 0, 1
+        
+        # even
+        for i, char in enumerate(s):
+            print(left,right)
+            while(left >= 0 and right <= length-1):
+                if s[left]==s[right]:
+                    result += 1
+                    left -= 1
+                    right += 1
+                else:
+                    break
+            
+            left, right = i+1, i+2
+        
+        
+        return result
+        
